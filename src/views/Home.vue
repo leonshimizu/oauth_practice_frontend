1b540360921b21c6663f837cda54c54e29b02f54
“leonshimizu” <lmshimizu@gmail.com>
2021-12-01 16:22:10 -0800

    todos index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <ul>
+      <li v-for="todo in todos">
+        {{ todo }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <style></style>
 
 <script>
+  import axios from 'axios'
   export default {
     data: function () {
       return {
         message: "Welcome to Vue.js!",
+        todos: []
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+      this.indexFunction();
+    },
+    methods: {
+      indexFunction: function() {
+        console.log("in the index function");
+        axios 
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .then(response => {
+            console.log(response.data);
+            this.todos = response.data;
+          })
+      }
+    },
   };
 </script>
