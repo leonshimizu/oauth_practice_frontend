07f2ab26adc2c0b7f544cf84e66d310c4f669124
“leonshimizu” <lmshimizu@gmail.com>
2021-12-01 17:12:13 -0800

    news index

---
+++ src/views/NewsIndex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="news">
+    <h1>{{ message }}</h1>
+    <ul>
+      <li v-for="post in posts.articles">
+        {{ post }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from 'axios';
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to the News!",
+        posts: []
+      };
+    },
+    created: function () {
+      this.newsIndex();
+    },
+    methods: {
+      newsIndex: function() {
+        console.log("in the news index");
+        axios
+          .get('/news')
+          .then(response => {
+            console.log(response.data);
+            this.posts = response.data;
+          })
+      }
+    },
+  };
+</script>
